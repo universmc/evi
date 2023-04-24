922534c66abe1ade4370bec1956f3f93be99a1e4
universmc <45916820+universmc@users.noreply.github.com>
2023-04-24 09:59:24 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<doctype html; xml; php>
+	<html>
+		<head>
+			<meta>
+      <title>
+      <linkk>
+    </head>
+		<body>
+        <header><header>
+        <footer></footer>
+        <script></script>
+    </body>
+  </html>
